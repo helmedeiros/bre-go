2220fbfd196ecb400744b9a7a01c8258ef6a567a
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-02-08 14:48:18 -0300

    Add runnable example for firstmatch.Engine
    Three-tier pricing scenario (premium / standard / default) is the
    canonical first-match shape: rule order encodes precedence, the
    first matching rule wins, the default catches everything else.
    Compile-checked godoc that doubles as a smoke test for the
    last-rule-as-catch-all pattern.

---
+++ engine/firstmatch/example_test.go
@@ -0,0 +1,32 @@
+package firstmatch_test
+
+import (
+	"fmt"
+
+	"github.com/helmedeiros/bre-go/engine"
+	"github.com/helmedeiros/bre-go/engine/firstmatch"
+)
+
+func ExampleEngine() {
+	e := firstmatch.New()
+	_ = e.AddRule(firstmatch.Rule{
+		Name:      "premium",
+		Condition: func(in interface{}) bool { return in.(int) >= 1000 },
+		Action:    func(in interface{}) interface{} { return "premium-tier" },
+	})
+	_ = e.AddRule(firstmatch.Rule{
+		Name:      "standard",
+		Condition: func(in interface{}) bool { return in.(int) >= 100 },
+		Action:    func(in interface{}) interface{} { return "standard-tier" },
+	})
+	_ = e.AddRule(firstmatch.Rule{
+		Name:      "default",
+		Condition: func(interface{}) bool { return true },
+		Action:    func(interface{}) interface{} { return "default-tier" },
+	})
+
+	res, _ := e.Execute(engine.Request{Input: 250})
+
+	fmt.Println(res.Matched, res.Output)
+	// Output: [standard] standard-tier
+}
