a51e0f5dc500ca49f359b8b887e77a1bab192e1a
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-02-15 09:24:42 -0300

    Add runnable examples showing combinators in a rule
    Two godoc examples wiring engine/conditions through an
    inmemory.Engine on a small 'order' domain. The first uses
    And + Not for a multi-condition approval rule; the second uses
    Or for a currency allowlist. Both compile-check the integration
    between the combinator package and the adapter package, and
    demonstrate that combinator-produced predicates drop into
    Rule.Condition with no conversion.

---
+++ engine/conditions/example_test.go
@@ -0,0 +1,62 @@
+package conditions_test
+
+import (
+	"fmt"
+
+	"github.com/helmedeiros/bre-go/engine"
+	"github.com/helmedeiros/bre-go/engine/conditions"
+	"github.com/helmedeiros/bre-go/engine/inmemory"
+)
+
+type order struct {
+	amount   int
+	currency string
+	flagged  bool
+}
+
+func amountOver(n int) func(interface{}) bool {
+	return func(in interface{}) bool { return in.(order).amount > n }
+}
+
+func currencyIs(c string) func(interface{}) bool {
+	return func(in interface{}) bool { return in.(order).currency == c }
+}
+
+func flagged(in interface{}) bool { return in.(order).flagged }
+
+func ExampleAnd() {
+	e := inmemory.New()
+	_ = e.AddRule(inmemory.Rule{
+		Name: "high-value-clean-usd",
+		Condition: conditions.And(
+			amountOver(100),
+			currencyIs("USD"),
+			conditions.Not(flagged),
+		),
+		Action: func(interface{}) interface{} { return "approve" },
+	})
+
+	clean := order{amount: 250, currency: "USD", flagged: false}
+	res, _ := e.Execute(engine.Request{Input: clean})
+
+	fmt.Println(res.Matched, res.Output)
+	// Output: [high-value-clean-usd] approve
+}
+
+func ExampleOr() {
+	e := inmemory.New()
+	_ = e.AddRule(inmemory.Rule{
+		Name: "any-major-currency",
+		Condition: conditions.Or(
+			currencyIs("USD"),
+			currencyIs("EUR"),
+			currencyIs("BRL"),
+		),
+		Action: func(interface{}) interface{} { return "supported" },
+	})
+
+	res, _ := e.Execute(engine.Request{Input: order{currency: "EUR"}})
+
+	fmt.Println(res.Matched, res.Output)
+	// Output: [any-major-currency] supported
+}
