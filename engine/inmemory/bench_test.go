69063ff7ae024214e39291903042ab6b216a3068
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-03-10 15:38:18 -0300

    Benchmark the recovery-wrapper overhead on a no-op Action
    Baseline number for the defer-recover cost on the success path.
    ~64 ns/op on the bench host with one allocation (the engine.Result
    slice). Anyone who later optimizes the recovery path -- moving it
    behind a flag, batching the defer across rules, etc -- can compare
    against this number.

--- engine/inmemory/bench_test.go
+++ engine/inmemory/bench_test.go
@@ -20,6 +20,23 @@ func BenchmarkExecuteOverTenRules(b *testing.B) {
 	}
 }
 
+func BenchmarkExecuteRecoveryOverhead(b *testing.B) {
+	e := inmemory.New()
+	_ = e.AddRule(inmemory.Rule{
+		Name:      "noop",
+		Condition: func(interface{}) bool { return true },
+		Action:    func(in interface{}) interface{} { return in },
+	})
+	req := engine.Request{Input: 5}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := e.Execute(req); err != nil {
+			b.Fatalf("Execute: unexpected error: %v", err)
+		}
+	}
+}
+
 func BenchmarkExecuteWithListenerOverTenRules(b *testing.B) {
 	e := tenRuleEngine()
 	e.AddListener(observability.NopExecutionListener{})
