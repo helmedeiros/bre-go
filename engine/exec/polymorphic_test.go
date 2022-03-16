159f2f5da840d5995eab4888f3f0d5a3ce78a838
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-03-16 15:38:18 -0300

    Verify Executor wraps both adapters interchangeably
    Same Executor[int, string] over inmemory and firstmatch, both
    returning 'answer' on input 42. Zero adapter-specific code in
    the assertion body -- the test treats both engines through
    engine.Engine alone and the wrapper handles the typing. The
    testimony that ADR-0013's 'sit over any engine.Engine, do not
    change the adapters' design held under a real second wiring.

---
+++ engine/exec/polymorphic_test.go
@@ -0,0 +1,62 @@
+package exec_test
+
+import (
+	"testing"
+
+	"github.com/helmedeiros/bre-go/engine"
+	"github.com/helmedeiros/bre-go/engine/exec"
+	"github.com/helmedeiros/bre-go/engine/firstmatch"
+	"github.com/helmedeiros/bre-go/engine/inmemory"
+)
+
+func TestExecutorWrapsBothAdapters(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		seed func(t *testing.T) engine.Engine
+	}{
+		{name: "inmemory", seed: seedTypedInmemory},
+		{name: "firstmatch", seed: seedTypedFirstmatch},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			ex := exec.New[int, string](tc.seed(t))
+
+			out, matched, err := ex.Execute(42)
+			if err != nil {
+				t.Fatalf("Execute: unexpected error: %v", err)
+			}
+			if out != "answer" {
+				t.Fatalf("out: want %q, got %q", "answer", out)
+			}
+			if len(matched) != 1 || matched[0] != "the-answer" {
+				t.Fatalf("matched: want [the-answer], got %v", matched)
+			}
+		})
+	}
+}
+
+func seedTypedInmemory(t *testing.T) engine.Engine {
+	t.Helper()
+	e := inmemory.New()
+	if err := e.AddRule(inmemory.Rule{
+		Name:      "the-answer",
+		Condition: func(in interface{}) bool { return in.(int) == 42 },
+		Action:    func(interface{}) interface{} { return "answer" },
+	}); err != nil {
+		t.Fatalf("inmemory.AddRule: unexpected error: %v", err)
+	}
+	return e
+}
+
+func seedTypedFirstmatch(t *testing.T) engine.Engine {
+	t.Helper()
+	e := firstmatch.New()
+	if err := e.AddRule(firstmatch.Rule{
+		Name:      "the-answer",
+		Condition: func(in interface{}) bool { return in.(int) == 42 },
+		Action:    func(interface{}) interface{} { return "answer" },
+	}); err != nil {
+		t.Fatalf("firstmatch.AddRule: unexpected error: %v", err)
+	}
+	return e
+}
