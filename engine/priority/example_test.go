1ea16cfea3e453fab3dedaf368e4ad2c0b40a265
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-03-22 14:48:42 -0300

    Add runnable example for priority.Engine
    Three-row decision table: a low-priority default catch-all, a
    mid-priority VIP rule, and a high-priority blocklist guard. The
    example proves the headline shape -- priority drives evaluation
    order, not insertion order. Inserted default first, vip second,
    blocklist third; on a vip-token input the vip rule wins because
    priority 100 beats priority 0 even though the default would
    also match.

---
+++ engine/priority/example_test.go
@@ -0,0 +1,36 @@
+package priority_test
+
+import (
+	"fmt"
+
+	"github.com/helmedeiros/bre-go/engine"
+	"github.com/helmedeiros/bre-go/engine/priority"
+)
+
+func ExampleEngine() {
+	e := priority.New()
+
+	_ = e.AddRule(priority.Rule{
+		Name:      "default",
+		Priority:  0,
+		Condition: func(interface{}) bool { return true },
+		Action:    func(interface{}) interface{} { return "standard-tier" },
+	})
+	_ = e.AddRule(priority.Rule{
+		Name:      "vip",
+		Priority:  100,
+		Condition: func(in interface{}) bool { return in.(string) == "vip-token" },
+		Action:    func(interface{}) interface{} { return "vip-tier" },
+	})
+	_ = e.AddRule(priority.Rule{
+		Name:      "blocklisted",
+		Priority:  1000,
+		Condition: func(in interface{}) bool { return in.(string) == "blocked-token" },
+		Action:    func(interface{}) interface{} { return "denied" },
+	})
+
+	res, _ := e.Execute(engine.Request{Input: "vip-token"})
+
+	fmt.Println(res.Matched, res.Output)
+	// Output: [vip] vip-tier
+}
