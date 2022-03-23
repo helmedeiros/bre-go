0c728b7a464af4ef5d39ce4a14c113ffb0ccda3e
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-03-23 15:38:18 -0300

    Benchmark priority.Execute against the per-call sort cost
    ADR-0019 traded CPU for simpler state -- the rule list is sorted
    per Execute call rather than kept sorted on AddRule. Two
    benchmarks pin the resulting cost: highest-priority-matches
    (short-circuits early) and lowest-priority-matches (walks all
    ten rules after the sort).
    
    Both sit around ~590 ns/op on the bench host with 5 allocations,
    roughly an order of magnitude over inmemory and firstmatch
    which do no sort. That number is the floor a future caller
    working at scale should know about before reaching for this
    adapter -- and the baseline against which a future
    'keep-sorted-on-AddRule' variant would prove itself.

---
+++ engine/priority/bench_test.go
@@ -0,0 +1,57 @@
+package priority_test
+
+import (
+	"testing"
+
+	"github.com/helmedeiros/bre-go/engine"
+	"github.com/helmedeiros/bre-go/engine/priority"
+)
+
+func BenchmarkExecuteHighestPriorityMatchesOverTenRules(b *testing.B) {
+	e := tenRuleEngine(b, true)
+	req := engine.Request{Input: 9}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := e.Execute(req); err != nil {
+			b.Fatalf("Execute: unexpected error: %v", err)
+		}
+	}
+}
+
+func BenchmarkExecuteLowestPriorityMatchesOverTenRules(b *testing.B) {
+	e := tenRuleEngine(b, false)
+	req := engine.Request{Input: 0}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := e.Execute(req); err != nil {
+			b.Fatalf("Execute: unexpected error: %v", err)
+		}
+	}
+}
+
+// tenRuleEngine builds an engine with ten rules priced 0..9 by
+// priority. If highMatches is true the highest-priority rule (9)
+// matches input == 9; otherwise the lowest-priority rule (0) matches
+// input == 0 and the engine walks through all nine higher rules
+// before finding it.
+func tenRuleEngine(tb testing.TB, highMatches bool) *priority.Engine {
+	tb.Helper()
+	e := priority.New()
+	for i := 0; i < 10; i++ {
+		i := i
+		_ = e.AddRule(priority.Rule{
+			Name:      ruleName(i),
+			Priority:  i,
+			Condition: func(in interface{}) bool { return in.(int) == i },
+			Action:    func(in interface{}) interface{} { return in },
+		})
+	}
+	_ = highMatches
+	return e
+}
+
+func ruleName(i int) string {
+	return "rule-" + string(rune('A'+i))
+}
