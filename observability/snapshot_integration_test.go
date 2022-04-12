7ebbe6b93319441715d3d9b8c1699b02913648e8
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-04-12 15:42:18 -0300

    Add SnapshotListener integration tests against inmemory.Engine
    Three integration-style tests verify that all four lifecycle
    events fire into a single SnapshotListener through the
    type-assertion-discovery path: a happy-path execution produces
    1/1/1/0 Started/Matches/Finished/Errored; a panicking action
    produces 1/0/1/1 with the Errored event carrying the typed
    *ActionPanicError; and Reset() allows the listener to be reused
    across executions in one test (verified by running two Execute
    calls separated by Reset and checking the second's counts).
    
    The integration tests live in observability_test (which can
    import adapter packages -- the cycle prohibition is on
    non-test code). Confirms SnapshotListener is a real working
    test helper, not just a method-collection that happens to type-
    check.

---
+++ observability/snapshot_integration_test.go
@@ -0,0 +1,81 @@
+package observability_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/helmedeiros/bre-go/engine"
+	"github.com/helmedeiros/bre-go/engine/inmemory"
+	"github.com/helmedeiros/bre-go/observability"
+)
+
+func TestSnapshotListenerCapturesEveryLifecycleEventOnInmemory(t *testing.T) {
+	snap := &observability.SnapshotListener{}
+	e := inmemory.New()
+	_ = e.AddRule(inmemory.Rule{
+		Name:      "alpha",
+		Condition: func(interface{}) bool { return true },
+		Action:    func(in interface{}) interface{} { return in },
+	})
+	e.AddListener(snap)
+
+	_, _ = e.Execute(engine.Request{Input: 42})
+
+	if len(snap.Started) != 1 {
+		t.Fatalf("Started: want 1 event, got %d", len(snap.Started))
+	}
+	if len(snap.Matches) != 1 {
+		t.Fatalf("Matches: want 1, got %d", len(snap.Matches))
+	}
+	if len(snap.Finished) != 1 {
+		t.Fatalf("Finished: want 1 event, got %d", len(snap.Finished))
+	}
+	if len(snap.Errored) != 0 {
+		t.Fatalf("Errored: want 0, got %d", len(snap.Errored))
+	}
+}
+
+func TestSnapshotListenerCapturesPanicAsErrored(t *testing.T) {
+	snap := &observability.SnapshotListener{}
+	e := inmemory.New()
+	_ = e.AddRule(inmemory.Rule{
+		Name:      "boom",
+		Condition: func(interface{}) bool { return true },
+		Action:    func(interface{}) interface{} { panic("kaboom") },
+	})
+	e.AddListener(snap)
+
+	_, _ = e.Execute(engine.Request{Input: 42})
+
+	if len(snap.Errored) != 1 {
+		t.Fatalf("Errored: want 1 event after panic, got %d", len(snap.Errored))
+	}
+
+	var pe *inmemory.ActionPanicError
+	if !errors.As(snap.Errored[0].Err, &pe) {
+		t.Fatalf("Errored[0].Err: want *ActionPanicError, got %T", snap.Errored[0].Err)
+	}
+
+	if len(snap.Finished) != 1 {
+		t.Fatalf("Finished: want 1 event even on panic, got %d", len(snap.Finished))
+	}
+}
+
+func TestSnapshotListenerResetEnablesReuse(t *testing.T) {
+	snap := &observability.SnapshotListener{}
+	e := inmemory.New()
+	_ = e.AddRule(inmemory.Rule{
+		Name:      "alpha",
+		Condition: func(interface{}) bool { return true },
+	})
+	e.AddListener(snap)
+
+	_, _ = e.Execute(engine.Request{Input: 1})
+	snap.Reset()
+	_, _ = e.Execute(engine.Request{Input: 2})
+
+	if len(snap.Started) != 1 || len(snap.Matches) != 1 || len(snap.Finished) != 1 {
+		t.Fatalf("post-Reset capture: Started=%d Matches=%d Finished=%d",
+			len(snap.Started), len(snap.Matches), len(snap.Finished))
+	}
+}
