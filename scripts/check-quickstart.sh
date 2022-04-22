f93d1c936615ec9bed049c5d8d82fc2e2b74687d
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-04-22 10:48:18 -0300

    Make check-quickstart find the first block with // Output:
    Bug from the v0.2.0 README rewrite: the new 'Upgrading from
    v0.1.0' section added a small ```go fenced block (before/after
    migration snippet) that became the first go fence in the file.
    check-quickstart picked it up, found no // Output: comment, and
    failed.
    
    Rewrite the awk to walk every ```go fenced block, keeping the
    first one whose body contains a '// Output:' comment. The
    migration snippet is now correctly ignored (it has no Output
    contract), and the real Quickstart further down the file is
    exercised.
    
    The Output-finding pass at line 34+ stays as is -- once the
    block is correctly selected, the rest of the script needs no
    changes.

--- scripts/check-quickstart.sh
+++ scripts/check-quickstart.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# Extract the first ```go ... ``` block from README.md, build and run it as
-# an external module that imports bre-go via a replace directive, and verify
-# the stdout matches the // Output: comment in the block. Catches drift
-# between the README example and the actual public API.
+# Extract the first ```go ... ``` block from README.md that contains a
+# `// Output:` comment, build and run it as an external module that
+# imports bre-go via a replace directive, and verify the stdout matches
+# the // Output: comment. Catches drift between the README example and
+# the actual public API.
 set -euo pipefail
 
 README="README.md"
@@ -24,10 +25,22 @@ require github.com/helmedeiros/bre-go v0.0.0
 replace github.com/helmedeiros/bre-go => $PWD
 EOF
 
-awk '/^```go$/{flag=1; next} /^```$/{if(flag){flag=0; exit}} flag' "$README" > "$workdir/main.go"
+# Walk every ```go ... ``` fenced block; keep the first one whose body
+# contains a "// Output:" comment.
+awk '
+  /^```go$/         { inblock=1; buf=""; hasoutput=0; next }
+  /^```$/           {
+    if (inblock) {
+      if (hasoutput) { print buf; exit }
+      inblock=0; buf=""; hasoutput=0
+    }
+    next
+  }
+  inblock           { buf = buf $0 "\n"; if (index($0, "// Output:")) hasoutput=1 }
+' "$README" > "$workdir/main.go"
 
 if [ ! -s "$workdir/main.go" ]; then
-  echo "no go code block found in $README" >&2
+  echo "no go code block with a // Output: comment found in $README" >&2
   exit 1
 fi
 
