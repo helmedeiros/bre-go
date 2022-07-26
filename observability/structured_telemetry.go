084941c318505dc6aadaed916a19fb1ff3da6d5a
helmedeiros <helio.cabralmedeiros@gmail.com>
2022-07-26 11:48:55 +0200

    Reduce comment density in indexed adapter, notifier, telemetry
    Apply the project's clean-code default ('write code that does not
    need a comment'): trim multi-paragraph block comments to one-line
    godoc on exports, remove inline tag-comments that restated the
    field's name, drop redundant cross-references to ADR numbers from
    method bodies.
    
    The reasoning that previously lived in comments is preserved in
    the ADRs themselves -- comments duplicated what the corresponding
    ADR already explained, and the duplication had started to drift
    (comments said 'see ADR-0029' / 'per ADR-0035' for fields whose
    behavior had since been refined in later ADRs).
    
    Trimmed:
    - engine/indexed/indexed.go: heaviest cut. Engine struct, Build,
      Built, sealLocked, readSnapshot, AddRule, RuleNames, RuleInfos,
      rulesView, Execute, collectSets, canonicalizeValues,
      cartesianFanout, enumerateCombinations, projectFact, coerceInput,
      passesPostFilter, factToInterfaceMap, WithPostFilterHook. Net
      -255 lines, no behavior change. Also removed a stray orphan
      comment fragment above the indexedRule struct definition.
    - engine/internal/adapter/notifier.go: Notifier doc + NotifyMatched
      / NotifyStarted / NotifyFinished / NotifyErrored godocs trimmed
      to one line each. Kept godoc on all exported methods (they're
      promoted onto every adapter via embedding -- godoc tooling needs
      to find them on the embedded type).
    - observability/structured_telemetry.go: TelemetryRecord,
      TelemetrySink, StructuredTelemetryListener,
      NewStructuredTelemetryListener docs trimmed; OnRuleMatched /
      OnExecutionStarted / OnExecutionFinished / OnExecutionErrored
      no longer carry per-method explanatory comments.
    
    ci-local green: vet, build, race-tests, 100% coverage, ADR index
    in sync, quickstart in sync.

--- observability/structured_telemetry.go
+++ observability/structured_telemetry.go
@@ -4,20 +4,8 @@ import (
 	"time"
 )
 
-// TelemetryRecord captures the lifecycle of a single Execute call
-// in a structured form. Emitted by StructuredTelemetryListener via
-// the caller-supplied TelemetrySink.
-//
-// Emission model: one record per terminal lifecycle event.
-//   - Success path: one record from OnExecutionFinished, Err nil.
-//   - Error path: two records -- one from OnExecutionErrored
-//     (Err non-nil, partial Output/Matched data), then one from
-//     OnExecutionFinished (Err nil, partial Output/Matched).
-//
-// Sinks that want exactly one record per Execute should
-// deduplicate by timestamp + Input pointer. Most consumers
-// (loggers, metrics counters) treat the events independently and
-// don't need correlation. See ADR-0038 for the design rationale.
+// TelemetryRecord captures one terminal lifecycle event of an Execute call.
+// Error path emits two records: one with Err set, one with Err nil.
 type TelemetryRecord struct {
 	Input    interface{}
 	Output   interface{}
@@ -26,30 +14,17 @@ type TelemetryRecord struct {
 	Err      error
 }
 
-// TelemetrySink consumes a structured telemetry record. Called
-// once per terminal lifecycle event by StructuredTelemetryListener.
-//
-// Sinks MUST be safe for concurrent calls when the engine is
-// built and Execute runs from multiple goroutines (the v0.12.0
-// concurrency model). The library does not synchronize the sink.
+// TelemetrySink consumes records. Must be safe for concurrent calls.
 type TelemetrySink func(TelemetryRecord)
 
-// StructuredTelemetryListener implements every observability
-// lifecycle interface and emits a TelemetryRecord per terminal
-// event via the configured sink. Wire it on any engine via
-// AddListener; the listener works identically across all four
-// adapters.
-//
-// Added in v0.13.0 per ADR-0038. The last Phase-4 parity-closure
-// piece.
+// StructuredTelemetryListener implements every lifecycle interface and
+// emits records via sink.
 type StructuredTelemetryListener struct {
 	sink TelemetrySink
 }
 
-// NewStructuredTelemetryListener returns a listener that emits
-// records via sink. Sink must not be nil; nil would silently
-// swallow telemetry, which is a wiring bug the constructor
-// surfaces immediately with panic.
+// NewStructuredTelemetryListener wires a sink. Panics on nil sink to surface
+// the wiring bug rather than silently dropping telemetry.
 func NewStructuredTelemetryListener(sink TelemetrySink) *StructuredTelemetryListener {
 	if sink == nil {
 		panic("observability: NewStructuredTelemetryListener called with nil sink")
@@ -57,18 +32,10 @@ func NewStructuredTelemetryListener(sink TelemetrySink) *StructuredTelemetryList
 	return &StructuredTelemetryListener{sink: sink}
 }
 
-// OnRuleMatched implements ExecutionListener. No-op; per-match
-// info is rolled up into TelemetryRecord.Matched at the terminal
-// event.
 func (l *StructuredTelemetryListener) OnRuleMatched(Match) {}
 
-// OnExecutionStarted implements ExecutionStartedListener. No-op;
-// the duration is delivered in the terminal event.
 func (l *StructuredTelemetryListener) OnExecutionStarted(interface{}) {}
 
-// OnExecutionFinished implements ExecutionFinishedListener.
-// Emits a TelemetryRecord with Err nil; Matched and Duration
-// reflect the values the engine passed.
 func (l *StructuredTelemetryListener) OnExecutionFinished(input, output interface{}, matched []string, duration time.Duration) {
 	l.sink(TelemetryRecord{
 		Input:    input,
@@ -78,10 +45,6 @@ func (l *StructuredTelemetryListener) OnExecutionFinished(input, output interfac
 	})
 }
 
-// OnExecutionErrored implements ExecutionErroredListener. Emits
-// a TelemetryRecord with Err set. Note that
-// OnExecutionFinished still fires after this; sinks see two
-// records for one errored Execute call. See ADR-0038 §2.
 func (l *StructuredTelemetryListener) OnExecutionErrored(input interface{}, err error) {
 	l.sink(TelemetryRecord{
 		Input: input,
